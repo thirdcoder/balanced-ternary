a9493d4efe10716703f49fe11f92439d0396411b
thirdcoder <boris3rd@bk.ru>
2015-07-03 12:18:02 -0700

    Enhance bt_cli to convert both n<->bts

--- bt_cli.js
+++ bt_cli.js
@@ -2,7 +2,23 @@
 var bts2n = require('./').bts2n;
 var n2bts = require('./').n2bts;
 
+// if the string could possibly represent balanced ternary
+function maybe_bts(s) {
+  return !!s.match(/^[01i]+$/);
+}
+
+// or a decimal number
+function maybe_n(s) {
+  return !!s.match(/^[-+]?[0-9]+$/);
+}
+
 process.argv.slice(2).forEach(function(arg) {
-  console.log(arg,'=',n2bts(arg));
+  if (maybe_n(arg)) {
+    console.log(n2bts(arg)+' = '+arg);
+  }
+
+  if (maybe_bts(arg)) {
+    console.log(arg+' = '+bts2n(arg));
+  }
 });
 
